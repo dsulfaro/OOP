7d2cae1ea85fee4b92b16c35d03290bb68586965
Dan Sulfaro <dsulfaro@nickdonn-mbp.local>
2017-02-10 14:58:54 -0800

    finish visits

--- URLShortener/app/models/shortened_url.rb
+++ URLShortener/app/models/shortened_url.rb
@@ -10,6 +10,15 @@ class ShortenedUrl < ActiveRecord::Base
     primary_key: :id
   )
 
+  has_many :visits,
+    class_name: "Visit",
+    foreign_key: :shortened_url_id,
+    primary_key: :id
+
+  has_many :visitors,
+    through: :visits,
+    source: :visitor
+
   def self.random_code
     code = SecureRandom::urlsafe_base64(16)
     while ShortenedUrl.exists?(short_url: code)
@@ -25,4 +34,22 @@ class ShortenedUrl < ActiveRecord::Base
       creator_id: user.id
     )
   end
+
+  def num_clicks
+    visits.count
+  end
+
+  def num_uniques
+    visitors.count
+  end
+
+  def num_recent_uniques
+    visits
+      .select("user_id")
+      .where("created_at > ?", 10.minutes.ago)
+      .distinct
+      .count
+  end
+
+
 end
--- URLShortener/app/models/user.rb
+++ URLShortener/app/models/user.rb
@@ -6,4 +6,15 @@ class User < ActiveRecord::Base
     foreign_key: :creator_id,
     primary_key: :id
 
+  has_many :visits,
+    class_name: "Visit",
+    foreign_key: :user_id,
+    primary_key: :id
+
+  has_many :visited_urls,
+    -> { distinct },
+    through: :visits,
+    source: :shortened_url
+
+
 end
