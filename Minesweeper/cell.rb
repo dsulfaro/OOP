af4be3d0e852f398ee8aac6c35697ca7b48c9329
Dan Sulfaro <dsulfaro@nickdonn-mbp.local>
2017-02-10 16:42:29 -0800

    start Minesweeper

---
+++ Minesweeper/cell.rb
@@ -0,0 +1,15 @@
+class Cell
+
+  attr_accessor :content
+  attr_reader :revealed?
+
+  def initialize(content = " ")
+    @content = content
+    @revealed? = false
+  end
+
+  def reveal
+    @revealed = true
+  end
+
+end
