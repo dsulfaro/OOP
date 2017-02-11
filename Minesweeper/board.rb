3b6e52b5c2ed265c44c76763afc7bd01d9d01e8f
Dan Sulfaro <dsulfaro@nickdonn-mbp.local>
2017-02-10 19:54:29 -0800

    board fixed

--- Minesweeper/board.rb
+++ Minesweeper/board.rb
@@ -8,6 +8,11 @@ class Board
     set_board!
   end
 
+  def [](*pos)
+    x, y = pos
+    @board[x][y]
+  end
+
   def render
     @board.each do |arr|
       arr.each do |cell|
@@ -19,6 +24,51 @@ class Board
 
   private
 
+  def set_numbers!(x, y)
+    if y > 0
+      @board[x][y - 1].set_number!
+      if x > 0
+        @board[x - 1][y - 1].set_number!
+      end
+      if x < @size - 1
+        @board[x + 1][y - 1].set_number!
+      end
+    end
+
+    if y < @size - 1
+      @board[x][y + 1].set_number!
+      if x > 0
+        @board[x - 1][y + 1].set_number!
+      end
+      if x < @size - 1
+        @board[x + 1][y + 1].set_number!
+      end
+    end
+
+    if x > 0
+      @board[x - 1][y].set_number!
+    end
+
+    if x < @size - 1
+      @board[x + 1][y].set_number!
+    end
+  end
+
+  def set_mines!
+    num_mines = @size
+    random_range = (0..@size - 1).to_a
+    until num_mines == 0
+      x = random_range.sample
+      y = random_range.sample
+      random_spot = @board[x][y]
+      unless random_spot.content == "M "
+        random_spot.set_tile!("M ")
+        set_numbers!(x, y)
+        num_mines -= 1
+      end
+    end
+  end
+
   def set_board!
     @size.times do
       row = []
@@ -27,8 +77,8 @@ class Board
       end
       @board << row
     end
+    set_mines!
   end
-
 end
 
 b = Board.new
--- Minesweeper/cell.rb
+++ Minesweeper/cell.rb
@@ -1,15 +1,22 @@
 class Cell
 
-  attr_accessor :content
-  attr_reader :revealed
+  attr_reader :revealed, :content
 
   def initialize(content = "[]")
     @content = content
     @revealed = false
   end
 
+  def set_tile!(stuff)
+    @content = stuff
+  end
+
   def reveal
     @revealed = true
+  end
+
+  def set_number!
+    @content = ((@content.to_i) + 1).to_s + " " if (@content == "[]" || @content.to_i > 0)
   end
 
 end
